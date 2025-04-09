e4151fee666980a74f1998e9c98e2b026f163edc
Markus Baumann <63606444+mksbaum@users.noreply.github.com>
2025-04-09 21:53:22 +0200

    feat: add mini.icons

--- init.lua
+++ init.lua
@@ -953,6 +953,7 @@ require('lazy').setup({
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
+      require('mini.icons').setup()
     end,
   },
   { -- Highlight, edit, and navigate code
